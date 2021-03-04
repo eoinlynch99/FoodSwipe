3cd800d5b47d2834bd71191341b6a8a834515d7f
eoinlynch99 <sterlynel@gmail.com>
2021-03-04 03:27:12 +0000

    Fixing maps issues.

--- app/src/main/java/com/fyp/foodswipe/settings.java
+++ app/src/main/java/com/fyp/foodswipe/settings.java
@@ -1,7 +1,7 @@
 package com.fyp.foodswipe;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -11,7 +11,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
@@ -20,29 +19,29 @@ import android.widget.Toast;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.protobuf.DescriptorProtos;
 
-import java.sql.DriverManager;
 
-public class settings extends FragmentActivity implements OnMapReadyCallback{
+public class settings extends FragmentActivity implements OnMapReadyCallback, OnMyLocationClickListener, ActivityCompat.OnRequestPermissionsResultCallback{
 
-    private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean permissionDenied = false;
+    private GoogleMap map;
     Location mUserLocation;
     LocationRequest mUserReq;
 
@@ -62,7 +61,7 @@ public class settings extends FragmentActivity implements OnMapReadyCallback{
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
         mUserReq = new LocationRequest();
         mUserReq.setInterval(1000);
@@ -71,7 +70,7 @@ public class settings extends FragmentActivity implements OnMapReadyCallback{
         mUserReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         // checks if the users version is better than marshmallow
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //checks if permission granted
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
 
@@ -86,42 +85,20 @@ public class settings extends FragmentActivity implements OnMapReadyCallback{
         @Override
         public void onLocationResult(LocationResult locationResult) {
             for(Location location : locationResult.getLocations()){
-                mUserLocation = location;
                 LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
 
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                map.animateCamera(CameraUpdateFactory.zoomTo(10));
 
                 // sending location to database
                 String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference(userID);
 
                 GeoFire geoFire = new GeoFire(ref);
-                geoFire.setLocation(userID, new GeoLocation(mUserLocation.getLatitude(), mUserLocation.getLongitude()));
+                geoFire.setLocation(userID, new GeoLocation(location.getLatitude(), location.getLongitude()));
             }
-
             }
         };
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case 1:{
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                        mFusedLocCl.requestLocationUpdates(mUserReq, mLocationCallback, Looper.myLooper());
-                        mMap.setMyLocationEnabled(true);
-                    }
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Please provide location permissions", Toast.LENGTH_LONG).show();
-                }
-                break;
-            }
-        }
-    }
 
     //double checks permissions
     private void checkLocationPermission() {
@@ -145,6 +122,30 @@ public class settings extends FragmentActivity implements OnMapReadyCallback{
                 ActivityCompat.requestPermissions(settings.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             }
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        mFusedLocCl.requestLocationUpdates(mUserReq, mLocationCallback, Looper.myLooper());
+                        map.setMyLocationEnabled(true);
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Please provide location permissions", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
     }
 }
 
