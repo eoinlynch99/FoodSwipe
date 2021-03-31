e79869c8cb73d5423f58c9ca2d2edfd10dd1cc0e
eoinlynch99 <sterlynel@gmail.com>
2021-03-31 20:58:10 +0100

    Shows nearby restaurants on map

--- app/src/main/java/com/fyp/foodswipe/MapsActivity.java
+++ app/src/main/java/com/fyp/foodswipe/MapsActivity.java
@@ -1,29 +1,17 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.fyp.foodswipe;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -31,161 +19,231 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MapsActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    private static final String TAG = MapsActivity.class.getSimpleName();
-    private GoogleMap map;
-    private CameraPosition cameraPosition;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    // initializing variables
+    Location deviceLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    int DEFAULT_ZOOM = 15;
+    private static final int REQUEST_CODE = 101;
+    boolean updateFinished;
+    private Marker[] placeMarkers;
+    private MarkerOptions[] places;
+    private GoogleMap gMap;
 
 
-    // The entry point to the Fused Location Provider.
-    private FusedLocationProviderClient fusedLocationProviderClient;
-
-    private static final int DEFAULT_ZOOM = 15;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean locationPermissionGranted;
-
-    // The geographical location where the device is currently located.
-    private Location lastKnownLocation;
-
-    // Keys for storing activity state.
-    private static final String KEY_CAMERA_POSITION = "camera_position";
-    private static final String KEY_LOCATION = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Retrieve location and camera position from saved instance state.
-        if (savedInstanceState != null) {
-            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
-        }
-
-        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
-
-
-        // Construct a FusedLocationProviderClient.
+        // initializes fusedLocationProviderClient to be used to get users location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        int MAX_PLACES = 20;
+        placeMarkers = new Marker[MAX_PLACES];
+        getLocation();
+    } // end onCreate
 
-        // Build the map.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-    }
-
-
-    //Saves the state of the map when the activity is paused.
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        if (map != null) {
-            outState.putParcelable(KEY_CAMERA_POSITION, map.getCameraPosition());
-            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
-        }
-        super.onSaveInstanceState(outState);
-    }
-
-
-
-    // this is used when the map is ready to be used
-    @Override
-    public void onMapReady(GoogleMap map) {
-        this.map = map;
-
-
-        // Prompt the user for permission.
-        getLocationPermission();
-
-        // Turn on the My Location layer and the related control on the map.
-        updateLocationUI();
-
-        // Get the current location of the device and set the position of the map.
-        getDeviceLocation();
-    }
-
-    // gets device location and moves camera
-    private void getDeviceLocation() {
-        try {
-            if (locationPermissionGranted) {
-                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            lastKnownLocation = task.getResult();
-                            if (lastKnownLocation != null) {
-                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                        new LatLng(lastKnownLocation.getLatitude(),
-                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                            }
-                        } else {
-                            Log.d(TAG, "No Current Location.");
-                            Log.e(TAG, "Exception: %s", task.getException());
-                        }
-                    }
-                });
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage(), e);
-        }
-    }
-
-    private void getLocationPermission() {
-        // requesting user location permissions
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            locationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }
-
-    //  handling the result of requesting user locations
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        locationPermissionGranted = false;
-        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                locationPermissionGranted = true;
-            }
-        }
-        updateLocationUI();
-    }
-
-
-    // updates map based on whether permissions were granted
-    private void updateLocationUI() {
-        if (map == null) {
+    private void getLocation() {
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
-        }
-        try {
-            if (locationPermissionGranted) {
-                map.setMyLocationEnabled(true);
-                map.getUiSettings().setMyLocationButtonEnabled(true);
-            } else {
-                map.setMyLocationEnabled(false);
-                map.getUiSettings().setMyLocationButtonEnabled(false);
-                lastKnownLocation = null;
-                getLocationPermission();
+        } // end if
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        // end onSuccess
+        task.addOnSuccessListener(location -> {
+            if(location != null) {
+                deviceLocation = location;
+                Toast.makeText(getApplicationContext(), deviceLocation.getLatitude() + "" + deviceLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                assert supportMapFragment != null;
+                supportMapFragment.getMapAsync(MapsActivity.this);
+            } // end if
+        }); // end task.addOnSuccessListener
+    } // end getLocation
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        double lat = deviceLocation.getLatitude();
+        double lng = deviceLocation.getLongitude();
+
+        LatLng latLng = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here!");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+
+
+
+        //build places query string
+        String placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
+                "json?location="+lat+","+lng+
+                "&radius=5000&sensor=true" +
+                "&types=restaurant"+
+                "&key=AIzaSyB55zVwYmfsg_5776MOyUU3-XOiGMAH4zc";
+
+        //execute query
+        googleMap.addMarker(markerOptions);
+        new GetPlaces().execute(placesSearchStr);
+    } // end onMapReady
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
             }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
+        } // end switch
+    } // end onRequestPermissionsResult
+
+    @SuppressLint("StaticFieldLeak")
+    private class GetPlaces extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... placesURL) {
+            //fetch places
+            updateFinished = false;
+            StringBuilder placesBuilder = new StringBuilder();
+            for (String placeSearchURL : placesURL) {
+                try {
+
+                    URL requestUrl = new URL(placeSearchURL);
+                    HttpURLConnection connection = (HttpURLConnection)requestUrl.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+                    int responseCode = connection.getResponseCode();
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                        BufferedReader reader;
+
+                        InputStream inputStream = connection.getInputStream();
+                        if (inputStream == null) {
+                            return "";
+                        }
+                        reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+
+                            placesBuilder.append(line).append("\n");
+                        }
+
+                        if (placesBuilder.length() == 0) {
+                            return "";
+                        }
+
+                        Log.d("test", placesBuilder.toString());
+                    }
+                    else {
+                        Log.i("test", "Unsuccessful HTTP Response Code: " + responseCode);
+                    }
+                } catch (MalformedURLException e) {
+                    Log.e("test", "Error processing Places API URL", e);
+                } catch (IOException e) {
+                    Log.e("test", "Error connecting to Places API", e);
+                }
+            }
+            return placesBuilder.toString();
+        }
+
+        //process data retrieved from doInBackground
+        protected void onPostExecute(String result) {
+            //parse place data returned from Google Places
+            //remove existing markers
+            if (placeMarkers != null) {
+                for (Marker placeMarker : placeMarkers) {
+                    if (placeMarker != null)
+                        placeMarker.remove();
+                }
+            }
+            try {
+                //parse JSON
+
+                //create JSONObject, pass stinrg returned from doInBackground
+                JSONObject resultObject = new JSONObject(result);
+                //get "results" array
+                JSONArray placesArray = resultObject.getJSONArray("results");
+                //marker options for each place returned
+                places = new MarkerOptions[((JSONArray) placesArray).length()];
+                //loop through places
+
+                Log.d("test", "The placesArray length is " + placesArray.length() + "...............");
+
+                for (int i = 0; i < placesArray.length(); i++) {
+                    //parse each place
+                    //if any values are missing we won't show the marker
+                    boolean missingValue;
+                    LatLng placeLL = null;
+                    String placeName = "";
+                    String vicinity = "";
+                    try {
+                        //attempt to retrieve place data values
+                        missingValue = false;
+                        //get place at this index
+                        JSONObject placeObject = placesArray.getJSONObject(i);
+                        //get location section
+                        JSONObject loc = placeObject.getJSONObject("geometry")
+                                .getJSONObject("location");
+                        //read lat lng
+                        placeLL = new LatLng(Double.parseDouble(loc.getString("lat")),
+                                Double.parseDouble(loc.getString("lng")));
+                        //get types
+                        JSONArray types = placeObject.getJSONArray("types");
+                        //vicinity
+                        vicinity = placeObject.getString("vicinity");
+                        //name
+                        placeName = placeObject.getString("name");
+                    } catch (JSONException jse) {
+                        Log.v("PLACES", "missing value");
+                        missingValue = true;
+                        jse.printStackTrace();
+                    }
+                    //if values missing we don't display
+                    if (missingValue) places[i] = null;
+                    else
+                        places[i] = new MarkerOptions()
+                                .position(placeLL)
+                                .title(placeName)
+                                .snippet(vicinity);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (places != null && placeMarkers != null) {
+                Log.d("test", "The placeMarkers length is " + placeMarkers.length + "...............");
+
+                for (int p = 0; p < places.length && p < placeMarkers.length; p++) {
+                    //will be null if a value was missing
+
+                    if (places[p] != null) {
+
+                        placeMarkers[p] = gMap.addMarker(places[p]);
+                    }
+                }
+            }
         }
     }
-}
+} // end onMapReady
